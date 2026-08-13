19376897a1b31186888597e986b0befa6792776a
209347269mm-svg <209347269.mm@gmail.com>
2026-08-13 13:06:33 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// הזרקת Tailwind CSS
-if (!document.getElementById("tailwind-script")) {
-  const script = document.createElement("script");
-  script.id = "tailwind-script";
-  script.src = "https://cdn.tailwindcss.com";
-  document.head.appendChild(script);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-// יצירת element במידה וחסר
-let rootElement = document.getElementById("root");
-if (!rootElement) {
-  rootElement = document.createElement("div");
-  rootElement.id = "root";
-  document.body.appendChild(rootElement);
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
